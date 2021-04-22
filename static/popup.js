023041e5534adadacbbc7c884f5c1b92059364fb
boredtea <es19btech11017@iith.ac.in>
2021-04-23 01:43:08 +0530

    Modify popup.js

--- static/popup.js
+++ static/popup.js
@@ -3,35 +3,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function onclick() {
         var data = document.getElementById("content").value;
-        var room_url = "https://gaussian-noise.herokuapp.com/party" + data;
-        fetch("/addRoom", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ content: data }),
-        })
-            .then((res) => res.json())
-            .then(() => {
-                // console.log(data);
-                chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-                    let msg = {
-                        txt: "start"
-                    }
-                    chrome.windows.create({
-                        tabId: tabs[0].id,
-                        type: 'popup',
-                        focused: true,
-                        // incognito, top, left, ...
-                        url: room_url
-                    });
-
-                    chrome.tabs.sendMessage(tabs[0].id, msg)
-                })
-                window.location.pathname = `/party/${data}`
-            })
-            .catch((err) => {
-                console.log(err);
+        var room_url = "https://gaussian-noise.herokuapp.com/party/" + data;
+        chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+            let msg = {
+                txt: "start"
+            }
+            chrome.windows.create({
+                tabId: tabs[0].id,
+                type: 'popup',
+                focused: true,
+                url: room_url
             });
+            chrome.tabs.sendMessage(tabs[0].id, msg)
+        })
     }
 }, false)
