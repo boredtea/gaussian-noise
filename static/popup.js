9b6c2b7d7927b0b6ef27fc9770a2f661184687be
boredtea <es19btech11017@iith.ac.in>
2021-04-22 23:57:15 +0530

    Broken extension code

---
+++ static/popup.js
@@ -0,0 +1,40 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.querySelector('#submit').addEventListener('click', onclick, false)
+
+//     function onclick() {
+//         console.log('hi');
+//         var data = document.getElementById("content").value;
+//         fetch("/addRoom", {
+//             method: "POST",
+//             headers: {
+//                 "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify({ content: data }),
+//         })
+//             .then((res) => res.json())
+//             .then((data) => {
+//                 console.log(data);
+//                 chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+//                     let msg = {
+//                         txt: "start"
+//                     }
+//                     //chrome.tabs.create({active: true, url: "https://gaussian-noise.herokuapp.com/"});
+//                     chrome.windows.create({
+//                         tabId: tabs[0].id,
+//                         type: 'popup',
+//                         focused: true,
+//                         // incognito, top, left, ...
+//                         url: "https://gaussian-noise.herokuapp.com/choose"
+//                     });
+        
+//                     chrome.tabs.sendMessage(tabs[0].id, msg)
+//                 })
+//                 window.location.pathname = '/party'
+//             })
+//             .catch((err) => {
+//                 console.log(err);
+//             });
+//     }
+// }, false)
+
+console.log("hlo madafaka");
--- templates/popup.js
+++ templates/popup.js
@@ -1,42 +1,42 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('button').addEventListener('click', onclick, false)
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#submit').addEventListener('click', onclick, false)
 
     function onclick() {
-        chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
-            let msg = {
-                txt: "start"
-            }
-            //chrome.tabs.create({active: true, url: "https://gaussian-noise.herokuapp.com/"});
-            chrome.windows.create({
-                tabId: tabs[0].id,
-                type: 'popup',
-                focused: true,
-                // incognito, top, left, ...
-                url: "https://gaussian-noise.herokuapp.com/choose"
-            });
-
-            chrome.tabs.sendMessage(tabs[0].id, msg)
-        })
-    }
-
-    var submit = document.getElementById("submit");
-        submit.onclick = function () {
-          var data = document.getElementById("content").value;
-          fetch("/addRoom", {
+        var data = document.getElementById("content").value;
+        fetch("/addRoom", {
             method: "POST",
             headers: {
-              "Content-Type": "application/json",
+                "Content-Type": "application/json",
             },
             body: JSON.stringify({ content: data }),
-          })
+        })
             //  console.log('hi')
             .then((res) => res.json())
             .then((data) => {
-              console.log(data);
-              window.location.pathname = '/party'
+                console.log(data);
+                chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+                    let msg = {
+                        txt: "start"
+                    }
+                    //chrome.tabs.create({active: true, url: "https://gaussian-noise.herokuapp.com/"});
+                    chrome.windows.create({
+                        tabId: tabs[0].id,
+                        type: 'popup',
+                        focused: true,
+                        // incognito, top, left, ...
+                        url: "https://gaussian-noise.herokuapp.com/choose"
+                    });
+        
+                    chrome.tabs.sendMessage(tabs[0].id, msg)
+                })
+                window.location.pathname = '/party'
             })
             .catch((err) => {
-              console.log(err);
+                console.log(err);
             });
-        };
+
+        
+    }
 }, false)
+
+// console.log("hlo madafaka");
