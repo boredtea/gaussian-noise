6eabe9814236d9342ec0fed112eeb3a081c9e7da
unknown <nandult07@gmail.com>
2021-04-23 07:49:59 +0530

    changed path of public rooms

--- extension/content.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.txt === "start") {
-
-        //sending student and course information to background.js
-        chrome.runtime.sendMessage({
-            dummy: "dummyText",
-        });
-    }
-});
